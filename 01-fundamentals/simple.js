66f764e3c0b59bab7c236e2d7a53ed70f2b1ae1f
Pudge Zhang <pudge@ciandt.com>
2022-07-31 10:07:06 +0800

    initial commit

---
+++ 01-fundamentals/simple.js
@@ -0,0 +1,9 @@
+const sum = (a, b) => a + b
+const subtract = (a, b) => a - b
+
+// simple example
+const result = sum(3, 7)
+const expected = 10
+if (result !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`)
+}
